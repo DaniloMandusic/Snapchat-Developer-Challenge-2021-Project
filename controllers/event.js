25c04d5a9fc7875b137ab64c5184117124e65efd
kubni <nikola.kuburovic123@gmail.com>
2021-10-22 18:40:40 +0200

    Second datetime field and date validation added

--- controllers/event.js
+++ controllers/event.js
@@ -8,11 +8,36 @@ function showPage(req,res,next)
 async function getEventInfo(req, res, next)
 {
     const postEventName = req.body.inputEventName;
-    const postEventTime = req.body.inputEventTime;
+    let postEventStartTime = req.body.inputEventStartTime;
+    let postEventEndTime = req.body.inputEventEndTime; // 2021-10-11T03:01
+    let postEventTime = "";
+    
+    // Date validation shenaningas
+    let startDatetime = new Date(postEventStartTime);
+    let endDatetime = new Date(postEventEndTime);
+    startDatetime.setSeconds(0, 0); // Seconds and milliseconds are irrelevant for comparison
+    endDatetime.setSeconds(0, 0);
+    const startDate = startDatetime.toLocaleDateString('de-DE');
+    const startTime = startDatetime.toLocaleTimeString(['de-DE'], {hour: '2-digit', minute:'2-digit'});
+    const endDate = endDatetime.toLocaleDateString('de-DE');
+    const endTime = endDatetime.toLocaleTimeString(['de-DE'], {hour: '2-digit', minute:'2-digit'}); 
+    
+    if(Date.parse(startDatetime.toLocaleDateString()) > Date.parse((endDatetime.toLocaleDateString()))) // Comparison in US format
+    {
+        postEventTime = "Error: start date > end date.";
+    }
+    else if(startTime > endTime)
+    {
+        postEventTime = "Error: start time > end time."
+    }
+    else if(Date.parse(startDatetime.toLocaleDateString()) == Date.parse((endDatetime.toLocaleDateString())))
+    {
+        postEventTime = `Event duration: ${startTime}h   -   ${endTime}h, ${startDate}`;
+    }
+    else postEventTime = `Event duration: ${startDate}, ${startTime}h   -    ${endTime}h, ${endDate}`;
+
     const postEventDescription = req.body.inputEventDescription;
     const postEventImage = req.file.filename;
-    // Ovde mozda provera baze podataka da se vidi da li su ovi podaci vec uneti bili u bazu
-    // radi zabrane duplikata
 
     // Add event to the database
     await model.addEvent(postEventName, postEventTime, postEventDescription, postEventImage);
