9548d59f811bf5c6d5435096d2c22a45a561779a
kubni <nikola.kuburovic123@gmail.com>
2021-10-25 15:21:12 +0200

    Date validation improved and moved to the frontend

--- controllers/event.js
+++ controllers/event.js
@@ -9,38 +9,15 @@ function makeEvent(req,res,next)
 {
     res.render("makeEvent.ejs");
 }
+
+
+
 async function getEventInfo(req, res, next)
 {
     try{
     const postEventName = req.body.inputEventName;
-    let postEventStartTime = req.body.inputEventStartTime;
-    let postEventEndTime = req.body.inputEventEndTime; // 2021-10-11T03:01
-    let postEventTime = "";
+    const postEventTime = req.body.eventTime;
     
-    // Date validation //
-    let startDatetime = new Date(postEventStartTime);
-    let endDatetime = new Date(postEventEndTime);
-    startDatetime.setSeconds(0, 0); // Seconds and milliseconds are irrelevant for comparison
-    endDatetime.setSeconds(0, 0);
-    const startDate = startDatetime.toLocaleDateString('de-DE');
-    const startTime = startDatetime.toLocaleTimeString(['de-DE'], {hour: '2-digit', minute:'2-digit'});
-    const endDate = endDatetime.toLocaleDateString('de-DE');
-    const endTime = endDatetime.toLocaleTimeString(['de-DE'], {hour: '2-digit', minute:'2-digit'}); 
-    
-    if(Date.parse(startDatetime.toLocaleDateString()) > Date.parse((endDatetime.toLocaleDateString()))) // Comparison in US format
-    {
-        postEventTime = "Error: start date > end date.";
-    }
-    else if(Date.parse(startDatetime.toLocaleDateString()) == Date.parse((endDatetime.toLocaleDateString())))
-    {
-        // if(startTime > endTime)
-        // {
-        //     postEventTime = "Error: start time > end time."
-        // }
-        postEventTime = `Event duration: ${startTime}h   -   ${endTime}h, ${startDate}`;
-    }
-    else postEventTime = `Event duration: ${startDate}, ${startTime}h   -    ${endTime}h, ${endDate}`;
-    //////////////////////////////////////////////////////////////////////////////////////////////////
     const postEventDescription = req.body.inputEventDescription;
     const postEventImage = req.file.filename;
 
---
+++ public/dateValidation.js
@@ -0,0 +1,60 @@
+
+
+let customEventTime = "";
+
+function validateDatetime()
+{
+    const inputEventStartTime = document.getElementById("inputEventStartTime").value;
+    const inputEventEndTime = document.getElementById("inputEventEndTime").value;
+
+    // Date validation //
+    let startDatetime = new Date(inputEventStartTime);
+    let endDatetime = new Date(inputEventEndTime);
+    startDatetime.setSeconds(0, 0); // Seconds and milliseconds are irrelevant for comparison
+    endDatetime.setSeconds(0, 0);
+    const startDate = startDatetime.toLocaleDateString('de-DE'); 
+    const startTime = startDatetime.toLocaleTimeString(['de-DE'], {hour: '2-digit', minute:'2-digit'});
+    const endDate = endDatetime.toLocaleDateString('de-DE');
+    const endTime = endDatetime.toLocaleTimeString(['de-DE'], {hour: '2-digit', minute:'2-digit'}); 
+
+
+    if(Date.parse(startDatetime.toLocaleDateString()) > Date.parse((endDatetime.toLocaleDateString()))) // Comparison in US format
+    {
+        errorMsg = "Error: start date > end date.";
+        return false;
+    }
+    else if(Date.parse(startDatetime.toLocaleDateString()) == Date.parse((endDatetime.toLocaleDateString())))
+    {
+        if(startDatetime.getTime() >= endDatetime.getTime())
+        {
+            errorMsg = "Error: start time >= end time";
+            return false;
+        }
+        else 
+        {
+            customEventTime = `Event duration: ${startTime}h   -   ${endTime}h, ${startDate}`;
+            return true;
+        }
+    }
+    else 
+    {
+        customEventTime = `Event duration: ${startDate}, ${startTime}h   -    ${endTime}h, ${endDate}`;
+        return true;
+    }
+}
+
+const form = document.getElementById("makeEventForm");
+form.addEventListener("submit", function(ev){
+    if(validateDatetime() == false)
+    {
+        ev.preventDefault();
+        alert(`${errorMsg}`); 
+        return false;
+    }
+    else
+    {
+        let eventTime = document.getElementById("eventTime");
+        eventTime.value = customEventTime;
+        console.log("Event time: " + eventTime.value);
+    }
+});
